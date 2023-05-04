4f6c28a393dfcd025523644ea1a45c2daba252ed
altug okumus <altugokumus@gmail.com>
2023-05-05 00:30:12 +0300

    env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,9 +19,9 @@ const Contact = () => {
 </div>
 <footer>
         <div className='icons'>
-            <a target={"_blank"} href="https://www.linkedin.com/in/altu%C4%9F-okumu%C5%9F-a2304644/"><img width={25} src="linkedin_icon.png" alt="linkedin" /></a>
-            <a target={"_blank"} href="https://github.com/altugokumus"><img width={25} src="github_icon.png" alt="linkedin" /></a>
-            <a target={"_blank"} href="https://twitter.com/altgokms"><img width={25} src="twitter_icon.png" alt="linkedin" /></a>
+            <a target={"_blank"} rel="noreferrer" href="https://www.linkedin.com/in/altu%C4%9F-okumu%C5%9F-a2304644/"><img width={25} src="linkedin_icon.png" alt="linkedin" /></a>
+            <a target={"_blank"} rel="noreferrer" href="https://github.com/altugokumus"><img width={25} src="github_icon.png" alt="linkedin" /></a>
+            <a target={"_blank"} rel="noreferrer" href="https://twitter.com/altgokms"><img width={25} src="twitter_icon.png" alt="linkedin" /></a>
         </div>
         <p>Copyrights © 2023 Designed by Altug Okumus</p>
     </footer>
